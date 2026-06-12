12ca157b251324552d0ffc6fb3c1e0b81c25e4e8
MozerBlack <joao.mozer.santos@escola.pr.gov.br>
2026-06-12 14:37:12 +0000

    main

--- index.js
+++ index.js
@@ -8,4 +8,14 @@ let nome = "Pedro";
 console.log("Nome:", nome);
 console.log(typeof nome)
 
-//
+//Tipo Boolean
+let maiorDeIdade = true;
+console.log("Maior de Idade:", maiorDeIdade);
+console.log(typeof maiorDeIdade)
+
+//Tipo Undefined
+let endereço;
+console.log("Endereço:", endereço);
+console.log(typeof endereço)
+
+//Tipo Null
