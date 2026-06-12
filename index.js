cb27e99d3efbc445a615d591fcbdb5f4b9d0476e
MozerBlack <joao.mozer.santos@escola.pr.gov.br>
2026-06-12 14:05:08 +0000

    main

---
+++ index.js
@@ -0,0 +1,11 @@
+//Tipo Number
+let idade = 30;
+console.log("Idade:", idade);
+console.log(typeof idade)
+
+//Tipo String
+let nome = "Pedro";
+console.log("Nome:", nome);
+console.log(typeof nome)
+
+//
